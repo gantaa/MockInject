ea1dbded2c94ec0855d8f59183bff3c99442f4ce
Matthew Ganski <gantasygames@gmail.com>
2013-04-09 13:29:00 -0700

    Initial Commit

---
+++ MockInject/MockInject.m
@@ -0,0 +1,13 @@
+//
+//  MockInject.m
+//  MockInject
+//
+//  Created by Matt Ganski on 4/9/13.
+//  Copyright (c) 2013 FitNerds. All rights reserved.
+//
+
+#import "MockInject.h"
+
+@implementation MockInject
+
+@end
